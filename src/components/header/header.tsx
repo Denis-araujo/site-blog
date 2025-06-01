00f7cdc476a3bfc95002ba3b99cac0318f7fd9ad
Denis-araujo <denisgomes@febracis.com.br>
2025-06-01 09:38:55 -0300

    feature: seção de melhoria

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link"
-import { Button } from "../ui/button"
-import { ActiveLink } from "../active-link";
-import { Logo } from "../logo";
+import Link from 'next/link';
+import { Logo } from '../logo';
+import { ActiveLink } from '../active-link';
+import { Button } from '../ui/button';
 
 export const Header = () => {
-
   return (
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background-60">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-8">
         <div className="flex h-16 items-center justify-between">
           <Logo />
 
@@ -15,13 +14,11 @@ export const Header = () => {
             <ActiveLink href="/">Início</ActiveLink>
             <ActiveLink href="/blog">Blog</ActiveLink>
             <Button variant="secondary" asChild>
-              <Link href="/comecar">
-                Começar
-              </Link>
+              <Link href="/comecar">Começar</Link>
             </Button>
           </nav>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
