9c3e331e6134f5548f4c009168f296df2a3d813d
Denis-araujo <denisgomes@febracis.com.br>
2025-06-05 09:52:18 -0300

    feature: adicionado seo na landpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,25 @@
+import type { Metadata } from "next";
 import { LandPage } from "@/templates/land-page";
+
+export const metadata: Metadata = {
+  title: "Site.Set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "",
+    url: "https://site-blog-opal-gamma.vercel.app/og-image.png",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [{
+      url: "https://site-blog-opal-gamma.vercel.app/og-image.png",
+      width: 800,
+      height: 600,
+      alt: "Site.Set"
+    }]
+  }
+}
 
 export default function HomePage() {
   return (
